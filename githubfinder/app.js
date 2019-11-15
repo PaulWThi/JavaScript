1193060f22fa5cd34fad7805bd6882c198aa6339
Paul <Paul.w.thi@gmail.com>
2019-11-15 15:41:18 -0600

    Add functionalities and UI

--- githubfinder/app.js
+++ githubfinder/app.js
@@ -16,6 +16,7 @@ searchUser.addEventListener('keyup', (e) => {
           // Show profile
           ui.clearAlert();
           ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
         }
       })
     } else {
